0272c44ec3e0fc8b9a9345f62ba0cd52014a69a5
Prayush Khadka <khadkaprayush8@gmail.com>
2025-05-19 09:06:54 +0545

    practicing small programs

---
+++ pythonpractice.py
@@ -0,0 +1,25 @@
+print("Hello Prayush")
+print("""Hi 
+      gang""")
+print("*"*5)
+name="Prayush"
+print(name)
+naam=input("Enter your name: ")
+print("Hello "+ naam)
+color=input("What is your fav color?")
+print(name+" likes "+color)
+
+weight=input("Enter your weight in pounds: ")
+weight_kgs=float(weight)*0.45
+print("Weight in kgs is ", weight_kgs)
+
+string='hi'
+hello=string
+print(hello)
+print(f'hello {hello}')
+
+course='python'
+print(course.replace('p','q'))
+import math
+math.ceil(2.7)
+math.floor(2.9)
